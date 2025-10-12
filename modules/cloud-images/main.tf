ba442c4aa09693d83b85b48ff9f7e8ffea58b5bb
kiprotichgidii <gedionkiprotich3@gmail.com>
2025-10-12 22:56:21 +0300

    Update main.tf file

---
+++ modules/cloud-images/main.tf
@@ -0,0 +1,65 @@
+locals {
+  os = {
+    "ubuntu" = {
+      "latest" = "https://cloud-images.ubuntu.com/releases/noble/release/ubuntu-24.04-server-cloudimg-amd64.img",
+      "24.04"  = "https://cloud-images.ubuntu.com/releases/noble/release/ubuntu-24.04-server-cloudimg-amd64.img",
+      "22.04"  = "https://cloud-images.ubuntu.com/releases/jammy/release/ubuntu-22.04-server-cloudimg-amd64.img",
+      "20.04"  = "https://cloud-images.ubuntu.com/releases/focal/release/ubuntu-20.04-server-cloudimg-amd64-disk-kvm.img",
+      "18.04"  = "https://cloud-images.ubuntu.com/releases/bionic/release/ubuntu-18.04-server-cloudimg-amd64.img",
+      "16.04"  = "https://cloud-images.ubuntu.com/releases/xenial/release/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
+    },
+    "rockylinux" = {
+      "latest"   = "https://dl.rockylinux.org/pub/rocky/9/images/x86_64/Rocky-9-GenericCloud.latest.x86_64.qcow2",
+      "9"        = "https://dl.rockylinux.org/pub/rocky/9/images/x86_64/Rocky-9-GenericCloud.latest.x86_64.qcow2",
+      "8"        = "https://dl.rockylinux.org/pub/rocky/8/images/x86_64/Rocky-8-GenericCloud.latest.x86_64.qcow2"
+    },
+    "almalinux" = {
+      "latest"   = "https://repo.almalinux.org/almalinux/9/cloud/x86_64/images/AlmaLinux-9-GenericCloud-latest.x86_64.qcow2",
+      "9"        = "https://repo.almalinux.org/almalinux/9/cloud/x86_64/images/AlmaLinux-9-GenericCloud-latest.x86_64.qcow2",
+      "8"        = "https://repo.almalinux.org/almalinux/8/cloud/x86_64/images/AlmaLinux-8-GenericCloud-latest.x86_64.qcow2"
+    },
+    "oraclelinux" = {
+      "latest"   = "https://yum.oracle.com/templates/OracleLinux/OL9/u5/x86_64/OL9U5_x86_64-kvm-b253.qcow2",
+      "9"        = "https://yum.oracle.com/templates/OracleLinux/OL9/u5/x86_64/OL9U5_x86_64-kvm-b253.qcow2",
+      "8"        = "https://yum.oracle.com/templates/OracleLinux/OL8/u10/x86_64/OL8U10_x86_64-kvm-b237.qcow2"
+    },
+    "centos-stream" = {
+      "latest"   = "https://cloud.centos.org/centos/10-stream/x86_64/images/CentOS-Stream-GenericCloud-10-latest.x86_64.qcow2",
+      "10"       = "https://cloud.centos.org/centos/10-stream/x86_64/images/CentOS-Stream-GenericCloud-10-latest.x86_64.qcow2"
+      "9"        = "https://cloud.centos.org/centos/9-stream/x86_64/images/CentOS-Stream-GenericCloud-9-latest.x86_64.qcow2"
+    },
+    "debian" = {
+      "latest"    = "https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2",
+      "12"        = "https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2",
+      "11"        = "https://cloud.debian.org/images/cloud/bullseye/latest/debian-11-generic-amd64.qcow2",
+      "10"        = "https://cloud.debian.org/images/cloud/buster/latest/debian-10-generic-amd64.qcow2"
+    },
+    "fedora" = {
+      "latest"   = "https://download.fedoraproject.org/pub/fedora/linux/releases/42/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-42-1.1.x86_64.qcow2",
+      "42"       = "https://download.fedoraproject.org/pub/fedora/linux/releases/42/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-42-1.1.x86_64.qcow2",
+      "41"       = "https://download.fedoraproject.org/pub/fedora/linux/releases/41/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-41-1.4.x86_64.qcow2"
+    },
+    "alpine" = {
+      "latest"   = "https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/cloud/generic_alpine-3.21.2-x86_64-bios-tiny-r0.qcow2",
+      "3.21"     = "https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/cloud/generic_alpine-3.21.2-x86_64-bios-tiny-r0.qcow2",
+      "3.20"     = "https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/cloud/nocloud_alpine-3.20.5-x86_64-bios-tiny-r0.qcow2"
+    },
+    "amazonlinux" = {
+      "latest"   = "https://cdn.amazonlinux.com/al2023/os-images/2023.7.20250512.0/kvm/al2023-kvm-2023.7.20250512.0-kernel-6.1-x86_64.xfs.gpt.qcow2",
+      "2023"     = "https://cdn.amazonlinux.com/al2023/os-images/2023.7.20250512.0/kvm/al2023-kvm-2023.7.20250512.0-kernel-6.1-x86_64.xfs.gpt.qcow2"
+    },
+    "opensuse" = {
+      "latest"   = "https://download.opensuse.org/repositories/Cloud:/Images:/Leap_15.6/images/openSUSE-Leap-15.6.x86_64-NoCloud.qcow2",
+      "15.6"     = "https://download.opensuse.org/repositories/Cloud:/Images:/Leap_15.6/images/openSUSE-Leap-15.6.x86_64-NoCloud.qcow2",
+      "15.5"     = "https://download.opensuse.org/repositories/Cloud:/Images:/Leap_15.5/images/openSUSE-Leap-15.5.x86_64-NoCloud.qcow2"
+    },
+    "archlinux" = {
+      "latest"   = "https://geo.mirror.pkgbuild.com/images/latest/Arch-Linux-x86_64-cloudimg.qcow2"
+    },
+    "freebsd" = {
+      "latest"   = "https://object-storage.public.mtl1.vexxhost.net/swift/v1/1dbafeefbd4f4c80864414a441e72dd2/bsd-cloud-image.org/images/freebsd/14.2/2024-12-08/ufs/freebsd-14.2-ufs-2024-12-08.qcow2",
+      "14"       = "https://object-storage.public.mtl1.vexxhost.net/swift/v1/1dbafeefbd4f4c80864414a441e72dd2/bsd-cloud-image.org/images/freebsd/14.2/2024-12-08/ufs/freebsd-14.2-ufs-2024-12-08.qcow2",
+      "13"       = "https://object-storage.public.mtl1.vexxhost.net/swift/v1/1dbafeefbd4f4c80864414a441e72dd2/bsd-cloud-image.org/images/freebsd/13.4/2024-10-28/ufs/freebsd-13.4-ufs-2024-10-28.qcow2"
+    }
+  }
+}
--- modules/libvirt-vm/main.tf
+++ modules/libvirt-vm/main.tf
@@ -1,79 +1,235 @@
-# Terraform provider for libvirt
+##########################################
+# terraform-libvirt: VM Module
+# Purpose: Provision virtual machines on KVM/QEMU
+# Provider: terraform-provider-libvirt
+##########################################
 terraform {
   required_providers {
     libvirt = {
-      source = "dmacvicar/libvirt"
+      source  = "dmacvicar/libvirt"
+      version = "0.7.0"
+    }
+  }
+}
+provider "libvirt" {
+  uri = var.libvirt_uri
+}
+
+# ----------------------------------------------------------
+# Cloud Images module reference
+#----------------------------------------------------------
+module "cloud_images" {
+  source = "../cloud-images"
+  version = var.os_version
+  os_name = var.os_name
+  os_version = var.os_version
+}
+
+#----------------------------------------------------------
+# Libvirt Network Creation
+#----------------------------------------------------------
+resource "libvirt_network" "vm_network" {
+  count = var.create_network ? 1 : 0
+  name   = var.network_name
+  mode   = var.network_mode
+  autostart = var.autostart_network
+  mtu = var.network_mode != "bridge" ? var.network_mtu : null
+  addresses = var.network_mode != "bridge" ? [var.network_cidr] : null
+
+  dynamic "dhcp" {
+    for_each = var.network_mode == "bridge" && var.enable_dhcp ? [1] : []
+    content {
+      range_start = var.dhcp_range_start
+      range_end   = var.dhcp_range_end
     }
   }
 }
 
-provider "libvirt" {
-  uri ="qemu+ssh://root@192.168.1.100/system"
-}
-
-# Storage Pool Definition
+#----------------------------------------------------------
+# Storage Pool Creation
+#----------------------------------------------------------
 resource "libvirt_pool" "storage_pool" {
-  name = var.storage_pool_name
-  type = "dir"
+  count = var.create_storage_pool ? 1 : 0
+  name   = var.storage_pool_name
+  type   = var.pool_type_type
+  autostart = true
   target {
     path = var.storage_pool_path
   }
 }
 
+#----------------------------------------------------------
+# Random Resource Creation
+#----------------------------------------------------------
+resource "random_password" "root_password" {
+  count = var.set_root_password ? 1 : 0
+  length  = 16
+  special = true
+  override_special = "_%@"
+}
+
+resource "random_password" "user_password" {
+  count = var.set_user_password ? 1 : 0
+  length  = 16
+  special = true
+  override_special = "_%@"
+}
+
+resource "tls_private_key" "ssh_key" {
+  count = var.generate_ssh_key ? 1 : 0
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+#----------------------------------------------------------
+# Files Creation 
+#----------------------------------------------------------
+resource "local_sensitive_file" "root_password" {
+  count    = var.set_root_password ? 1 : 0
+  content  = random_password.root_password.result
+  filename = "${var.ssh_private_key_path}/${var.vm_name}_root_password.txt"
+  file_permission = "0600"
+}
+
+resource "local_sensitive_file" "user_password" {
+  count    = var.set_user_password ? 1 : 0
+  content  = random_password.user_password.result
+  filename = "${var.ssh_private_key_path}/${var.vm_name}_user_password.txt"
+  file_permission = "0600"
+}
+
+resource "local_sensitive_file" "ssh_private_key" {
+  count    = var.generate_ssh_key ? 1 : 0
+  content  = tls_private_key.ssh_key.private_key_pem
+  filename = "${var.ssh_private_key_path}/${var.vm_name}_id_rsa"
+  file_permission = "0600"
+}
+
+resource "local_sensitive_file" "ssh_public_key" {
+  count    = var.generate_ssh_key ? 1 : 0
+  content  = tls_private_key.ssh_key.public_key_openssh
+  filename = "${var.ssh_private_key_path}/${var.vm_name}_id_rsa.pub"
+  file_permission = "0644"
+}
+
+#----------------------------------------------------------
+# Create a libvirt volume for the base image
+#----------------------------------------------------------
+resource "libvirt_volume" "base_image" {
+  count = module.cloud_images.os.available ? 1 : 0
+  name   = "${var.vm_name}-base.qcow2"
+  pool   = var.storage_pool
+  source = module.cloud_images.os.url
+  format = "qcow2"
+}
+
+#----------------------------------------------------------
+# Create a copy-on-write volume for the VM
+#----------------------------------------------------------
 resource "libvirt_volume" "vm_disk" {
-  count  = length(var.disks)
-  name   = "${var.instance_name}-disk-${count.index}"
-  pool   = libvirt_pool.storage_pool.name
-  size   = var.disks[count.index].size
-  format = var.disk_format
+  count          = var.vm_disk
+  name           = "${var.vm_name}.qcow2"
+  base_volume_id = libvirt_volume.base_image.id
+  base_volume_name = libvirt_volume.base_image.name
+  pool           = var.storage_pool
+  format         = "qcow2"
+  size           = 1024 * 1024 * 1024 * var.disk_size
 }
 
-# Define network interfaces
-resource "libvirt_network" "network_interface" {
-  count = length(var.network_interfaces)
-  name  = var.network_interfaces[count.index].name
-  mode = "nat"
-  
-  # Define the network address range
-  addresses = ["192.168.2.0/24"]  # Use an IP range that doesn’t conflict with the default libvirt network
+#----------------------------------------------------------
+# Generate Cloud-Init ISO
+#----------------------------------------------------------
+data "template_file" "user_data" {
+  template = file("${path.root}/templates/cloud-init/user_data.tpl")
+
+  vars = {
+    timezone = var.timezone
+    enable_ssh_password = var.enable_ssh_password
+    disable_ssh_root_login = var.disable_ssh_root_login
+    lock_root_user_password = var.lock_root_user_password
+    set_root_password = var.set_root_password
+    root_password = local.root_password_hash
+    ssh_user_name = var.ssh_user_name
+    ssh_user_fullname = var.ssh_user_fullname
+    ssh_user_shell = var.ssh_user_shell
+    ssh_user_password = local.user_password_hash
+    set_ssh_user_password = var.set_user_password
+    ssh_authorized_keys = local.combined_ssh_keys
+    packages = var.packages
+    runcmds = var.runcmds
+    disable_ipv6 = var.disable_ipv6
+    package_update = var.package_update
+    package_upgrade = var.package_upgrade
+  }
 }
 
-# Define VM instances
+data "template_file" "network_config" {
+  template = file("${path.root}/templates/cloud-init/network_config.tpl")
+
+  vars = {
+    ip_address = var.ip_address
+    gateway    = var.ip_gateway
+    nic      = var.network_interface
+    dns        = var.dns_servers
+  }
+}
+
+data "template_file" "meta_data" {
+  template = file("${path.root}/templates/cloud-init/meta_data.tpl")
+
+  vars = {
+    instance_id = var.vm_name
+    local_hostname = var.vm_name
+  }
+}
+
+resource "libvirt_cloudinit_disk" "commoninit" {
+  name           = "${var.vm_name}-cloudinit.iso"
+  pool           = var.storage_pool
+  user_data      = data.template_file.user_data.rendered
+  meta_data      = data.template_file.meta_data.rendered
+  network_config = data.template_file.network_config.rendered
+}
+
+#----------------------------------------------------------
+# Create the Virtual Machine
+#----------------------------------------------------------
 resource "libvirt_domain" "vm" {
-  count  = var.instance_count
-  name   = "${var.instance_name}-${count.index}"
+  count = var.vm_count
+  name   = var.vm_name
   memory = var.memory
+  cpu_mode = var.cpu_mode
   vcpu   = var.vcpu
-
-  disk {
-    volume_id = libvirt_volume.vm_disk[count.index].id
-  }
-
-  # Attach bootable CD-ROM (ISO)
-  disk {
-    file   = var.cdrom_path # Path to the bootable ISO file
-  }
+  austostart = var.autostart_vm
+  qemu_agent = true
+  cloudinit = libvirt_cloudinit_disk.commoninit.id
 
   network_interface {
-    network_name = libvirt_network.network_interface[count.index].name
-  }
-
-  console {
-    type        = "pty"
-    target_port = "0"
-    target_type = "serial"
+    network_name = var.network_name 
+    wait_for_lease = true
   }
 
   graphics {
     type        = "vnc"
     listen_type = "address"
-    autoport    = "true"
-    listen_address = "0.0.0.0"  
+    autoport    = true
   }
 
-  boot_device {
-  dev = [ "cdrom", "hd", "network"]
-}
-  # Ensure the instance starts automatically
-  autostart = true
+  console {
+    type        = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
+
+  disk {
+    volume_id = libvirt_volume.vm_disk.id
+  }
+
+  depends_on = [libvirt_cloudinit_disk.commoninit]
 }
