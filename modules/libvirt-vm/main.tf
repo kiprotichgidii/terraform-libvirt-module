2ae8b1730614378f6f1c4f15a0d916ddad3bdecc
kiprotichgidii <gedionkiprotich3@gmail.com>
2025-10-30 22:57:01 +0300

    Update main.tf

--- modules/libvirt-vm/main.tf
+++ modules/libvirt-vm/main.tf
@@ -51,7 +51,7 @@ resource "random_password" "root_password" {
 
 resource "random_password" "user_password" {
   count            = var.set_user_password ? 1 : 0
-  length           = 16
+  length           = 5
   special          = true
   override_special = "_%@"
 }
